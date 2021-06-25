50e2203284e36bfd06a9c3484fee73b1d476c5db
zamfirflorin <37499220+zamfirflorin@users.noreply.github.com>
2021-06-25 13:09:30 +0300

    Add files via upload

---
+++ data-structures-and-algorithms/src/com/florin/FunctionalinterfacesAndPredicates/Main.java
@@ -0,0 +1,77 @@
+package FunctionalinterfacesAndPredicates;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee john = new Employee("John Doe", 30);
+        Employee tim = new Employee("Tim Buchalka", 21);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+        Employee red = new Employee("Red RidingHood", 35);
+        Employee charming = new Employee("Prince Charming", 31);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(tim);
+        employees.add(jack);
+        employees.add(snow);
+        employees.add(red);
+        employees.add(charming);
+
+        //by using function we can pass code that accepts and returns a value to a method in a form of  a lambda expression
+        //and then run that code without having to create an interface and a class that implements the interface
+        Function<Employee, String> getLastName = (Employee employee) -> {
+            return employee.getName().substring(employee.getName().indexOf(' ') + 1);
+        };
+
+        Function<Employee, String> getFirstName = (Employee employee) -> {
+          return employee.getName().substring(0, employee.getName().indexOf(' '));
+        };
+
+        String firstName = getFirstName.apply(employees.get(1));
+        //System.out.println(firstName);
+
+        String lastName = getLastName.apply(employees.get(1));
+        //System.out.println(lastName);
+
+        Random random1 = new Random();
+        for (Employee employee : employees) {
+            if (random1.nextBoolean()) {
+                System.out.println(getAName(getFirstName, employee));
+            } else {
+                System.out.println(getAName(getLastName, employee));
+            }
+        }
+
+        Function<Employee, String> upperCase = employee -> employee.getName().toUpperCase();
+        Function<String, String> firstNameUp = name -> name.substring(0, name.indexOf(' '));
+        Function chainedFunction = upperCase.andThen(firstNameUp);
+        System.out.println(chainedFunction.apply(employees.get(0)));
+
+    }
+
+    private static String getAName(Function<Employee, String> getName, Employee employee) {
+        return getName.apply(employee);
+    }
+
+    private static void printEmployeesByAge(List<Employee> employees,
+                                            String ageText,
+                                            Predicate<Employee> ageCondition) {
+
+        System.out.println(ageText);
+        System.out.println("==================");
+        for(Employee employee : employees) {
+            if (ageCondition.test(employee)) {
+                System.out.println(employee.getName());
+            }
+        }
+    }
+}
---
+++ data-structures-and-algorithms/src/com/florin/Lambda/Main.java
@@ -0,0 +1,126 @@
+package Lambda;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String args[]) {
+/*        new Thread(() -> {
+            System.out.println("Printing from the runnable");
+            System.out.println("Line 2");
+            System.out.format("This is line %d\n", 3);
+        }).start();*/
+
+        Employee john = new Employee("John Doe", 30);
+        Employee tim = new Employee("Tim", 21);
+        Employee florin = new Employee("Florin", 22);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(tim);
+        employees.add(florin);
+
+/*        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });*/
+
+        Collections.sort(employees, (employee1, employee2) -> employee1.getName().compareTo(employee2.getName()));
+
+/*
+        for (Employee employee: employees) {
+            System.out.println(employee.getName());
+        }
+*/
+
+        employees.forEach(employee -> {
+            System.out.println(employee.getName());
+            System.out.println(employee.getAge());
+        });
+
+/*        String sillyString = doStringStuff(new UpperConcat() {
+            @Override
+            public String upperAndConcat(String s1, String s2) {
+                return s1.toUpperCase() + s2.toUpperCase();
+            }
+        },
+                employees.get(0).getName(), employees.get(1).getName());
+        System.out.println(sillyString);*/
+/*
+        UpperConcat uc = (s1, s2) -> s1.toUpperCase() + s2.toUpperCase();
+        UpperConcat lc = (s1, s2) -> {
+            String result = s1.toLowerCase() + s2.toLowerCase();
+            return result;
+        };
+        UpperConcat ns = (String s1, String s2) -> s1.replace('i', 'c');
+        String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
+        String anotherString = doStringStuff(lc, employees.get(0).getName(), employees.get(1).getName());
+        String nsstring = doStringStuff(ns, employees.get(0).getName(), employees.get(1).getName());
+        System.out.println(sillyString);
+        System.out.println(anotherString);
+        System.out.println(nsstring);
+        AnotherClass anotherClass = new AnotherClass();
+        String s = anotherClass.doSomething();
+        System.out.println(s);*/
+    }
+    public final static String doStringStuff (UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
+    }
+}
+
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
+}
+
+class AnotherClass {
+    public String doSomething() {
+        UpperConcat uc = (s1, s2) -> {
+            System.out.println("The lambda experssions class is: " + getClass().getSimpleName());
+            String result = s1.toUpperCase() + s2.toUpperCase();
+            return result;
+        };
+        System.out.println("The anoterher class name is " + getClass().getSimpleName());
+        return Main.doStringStuff(uc, "String 1", "String 1");
+
+/*        System.out.println("The Another Class's name is: " + getClass().getSimpleName());
+        return Main.doStringStuff(new UpperConcat() {
+            @Override
+            public String upperAndConcat(String s1, String s2){
+                System.out.println("The Anonoymous Class's name is: " + getClass().getSimpleName());
+                return s1.toUpperCase() + s2.toUpperCase();
+            }
+        }, "String 1", "String 2");*/
+
+    }
+
+}
