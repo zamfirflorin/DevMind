9901fe60ed781d83a7e9b50e8d95c6562de5d207
FZAMFIR <FZAMFIR@USHY-vurmtO6evD.computacenter.com>
2021-08-05 23:44:43 +0300

    added Util class and refactored several methods

--- data-structures-and-algorithms/src/com/junior/SistemDeGestiune/Main.java
+++ data-structures-and-algorithms/src/com/junior/SistemDeGestiune/Main.java
@@ -8,39 +8,22 @@ public class Main {
 		System.out.println("Bun venit! Introduceti numarul de locuri disponibile:");
 		Scanner sc = new Scanner(System.in);
 		int availableSpots = sc.nextInt();
-		GuestsList guestList = createGuestsList(availableSpots);
+		GuestsList guestList = Utils.createGuestsList(availableSpots);
+		runCommands(guestList);
+	}
+	
+	public static void runCommands(GuestsList guestList) {
 		System.out.println("Asteapta comanda: (help - Afiseaza lista de comenzi)");
+		Scanner sc = new Scanner(System.in);
 		String command = sc.next();
 		while(!command.equals("quit")) {
-
-			getCommand(command);
+			getCommand(command, guestList);
 			command = sc.next();
 		}
-		
-		
-
-	}
-	
-	public static GuestsList createGuestsList (int availableSpots) {
-		GuestsList guestsList = new GuestsList(availableSpots);
-		return guestsList;
+		sc.close();
 	}
 
-	
-	public static void addGuest() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Introduceti numele de familie:");
-		String lastName = sc.next();
-		System.out.println("Introduceti prenumele:");
-		String firstName = sc.next();
-		System.out.println("Introduceti email:");
-		String email = sc.next();
-		System.out.println("Introduceti numar de telefon (format „+40733386463“):");
-		String phoneNumber = sc.next();
-	}
-	
-	
-	private static void getCommand(String command) {
+	private static void getCommand(String command, GuestsList guestList ) {
 		switch(command) {
 		case "help": 
 			System.out.println(""
@@ -59,38 +42,48 @@ public class Main {
 					+ "quit - Inchide aplicatia");
 			break;
 		case "add":
-			System.out.println("	Se adauga o noua persoana…)");
-			addGuest();
+			System.out.println("Se adauga o noua persoana…)");
+			Utils.addGuest(guestList);
 			break;
 		case "check": 
 			System.out.println("- Afiseaza aceasta lista de comenzi");
+			Utils.checkGuest(guestList);
 			break;
 		case "remove":
 			System.out.println("- Adauga o noua persoana (inscriere)");
+			Utils.removeGuest(guestList);
 			break;
 		case "update": 
 			System.out.println("- Afiseaza aceasta lista de comenzi");
+			Utils.updateGuest(guestList);
 			break;
 		case "guests":
 			System.out.println("- Adauga o noua persoana (inscriere)");
+			Utils.guests(guestList);
 			break;
 		case "waitlist": 
 			System.out.println("- Afiseaza aceasta lista de comenzi");
+			Utils.waitlist(guestList);
 			break;
 		case "available":
 			System.out.println("- Adauga o noua persoana (inscriere)");
+			Utils.available(guestList);
 			break;
 		case "guests_no": 
 			System.out.println("- Afiseaza aceasta lista de comenzi");
+			Utils.guests_no(guestList);
 			break;
 		case "waitlist_no":
 			System.out.println("- Adauga o noua persoana (inscriere)");
+			Utils.waitlist_no(guestList);
 			break;
 		case "subscribe_no": 
 			System.out.println("- Afiseaza aceasta lista de comenzi");
+			Utils.subscribe_no(guestList);
 			break;
 		case "search":
 			System.out.println("- Adauga o noua persoana (inscriere)");
+			Utils.search(guestList);
 			break;
 		case "quit":
 			System.out.println("- Adauga o noua persoana (inscriere)");
