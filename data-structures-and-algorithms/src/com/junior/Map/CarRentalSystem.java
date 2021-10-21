55092099f2b271d77223a252121f6d0080a3afd1
FZAMFIR <zamfirflorinstelian@yahoo.com>
2021-10-21 22:03:22 +0300

    implementing car rental system using hashmap

--- data-structures-and-algorithms/src/com/junior/Map/CarRentalSystem.java
+++ data-structures-and-algorithms/src/com/junior/Map/CarRentalSystem.java
@@ -22,5 +22,63 @@ public class CarRentalSystem {
 		return rentedCars.containsKey(plateNo);
 	}
 	
+	private String getCarRent(String plateNo) {
+		return rentedCars.get(plateNo);
+	}
+	
+	private void rentCar(String plateNo, String ownerName) {
+		rentedCars.put(plateNo, ownerName);
+	}
+	
+	private void returnCar(String plateNo) {
+		rentedCars.remove(plateNo);
+	}
+	
+	private static void printCommandsList() {
+		System.out.println("help         - Afiseaza aceasta lista de comenzi");
+		System.out.println("add          - Adauga o noua pereche (masina, sofer)");
+		System.out.println("check        - Verifica daca o masina este deja luata");
+		System.out.println("remove       - Sterge o masina existenta din hashtable");
+		System.out.println("getOwner     - Afiseaza proprietarul curent al masinii");
+		System.out.println("quit         - Inchide aplicatia");
+	}
+	
+	
+	public void run() {
+		boolean quit = false;
+		while (!quit) {
+			System.out.println("Asteapta comanda: (help - Afiseaza lista de comenzi)");
+			String command = sc.nextLine();
+			switch (command) {
+			case "help":
+				printCommandsList();
+				break;
+			case "add":
+				rentCar(getPlateNo(), getOwnerName());
+				break;
+			case "check":
+				System.out.println(isCarRented(getPlateNo()));
+				break;
+			case "remove":
+				returnCar(getPlateNo());
+				break;
+			case "getOwner":
+				System.out.println(getCarRent(getPlateNo()));
+				break;
+			case "quit":
+				System.out.println("Aplicatia se inchide...");
+				quit = true;
+				break;
+			default:
+				System.out.println("Unknown command. Choose from:");
+				printCommandsList();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		new CarRentalSystem().run();
+	}
+	
 	
 }
