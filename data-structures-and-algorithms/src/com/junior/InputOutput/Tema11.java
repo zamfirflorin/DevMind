bb50a11f5fc5f3e3ceb54bd65be1533b51a4122f
FZAMFIR <zamfirflorinstelian@yahoo.com>
2021-11-03 10:15:06 +0200

    changed the implementation

--- data-structures-and-algorithms/src/com/junior/InputOutput/Tema11.java
+++ data-structures-and-algorithms/src/com/junior/InputOutput/Tema11.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Scanner;
 
-public class Tema {
+public class Tema11 {
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -14,14 +14,14 @@ public class Tema {
 		String replacement = scanner.next();
 		
 		try (Scanner sc = new Scanner(new BufferedReader(new FileReader("TextInput.txt"))); 
-			BufferedWriter outputFile = new BufferedWriter(new FileWriter("output.txt"))) {
+			BufferedWriter outputFile = new BufferedWriter(new FileWriter("TextOutput.txt"))) {
 			
-			while(sc.hasNextLine()) {
+			while(sc.hasNext()) {
 				String currentWord = sc.next();
-				if (currentWord == firstWord) {
+				if (currentWord.equals(firstWord)) {
 					currentWord = replacement;
 				}
-				outputFile.write(currentWord);
+				outputFile.write(currentWord + " ");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
