2551f1ef4956192b731447f7a22a1ba4281a431b
FZAMFIR <zamfirflorinstelian@yahoo.com>
2021-11-05 12:22:46 +0200

    json parser

---
+++ data-structures-and-algorithms/src/com/junior/HighPerformanceEventCorrelation/ERROR_NOT_ARRAY.java
@@ -0,0 +1,5 @@
+package com.junior.HighPerformanceEventCorrelation;
+
+public class ERROR_NOT_ARRAY {
+
+}
--- data-structures-and-algorithms/src/com/junior/HighPerformanceEventCorrelation/MyJSONParser.java
+++ data-structures-and-algorithms/src/com/junior/HighPerformanceEventCorrelation/MyJSONParser.java
@@ -1,5 +1,24 @@
 package com.junior.HighPerformanceEventCorrelation;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class MyJSONParser {
+
+	public static void get(String... args) {
+
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String cmd;
+		ArrayList<String> commands = new ArrayList<>();
+		
+		
+		for (String s : commands) {
+			System.out.print(s + " ");
+		}
+	}
 
 }
