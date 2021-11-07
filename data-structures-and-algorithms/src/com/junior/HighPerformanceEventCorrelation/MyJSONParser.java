9281cd28600a898b5b4eacf79ea8f7ce52dce0d7
FZAMFIR <zamfirflorinstelian@yahoo.com>
2021-11-07 20:17:38 +0200

    json parser

--- data-structures-and-algorithms/src/com/junior/HighPerformanceEventCorrelation/MyJSONParser.java
+++ data-structures-and-algorithms/src/com/junior/HighPerformanceEventCorrelation/MyJSONParser.java
@@ -5,10 +5,28 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class MyJSONParser {
+	
+	public String json = "example.json";
+	public String input = "input.txt";
+	public String output = "output.txt";
 
 	public static void get(String... args) {
 
 	}
+	
+	public static void put(ArrayList<String> keys, Object value) {
+		
+	}
+	
+	public static void del(ArrayList<String> keys) { 
+		
+	}
+	
+	//citim de la tastatura un input de intrare
+	//daca comanda este get atunci facem un get
+	//daca comanda este put atunci facem un put
+	//daca comanda este del atunci facem un delete
+	
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
